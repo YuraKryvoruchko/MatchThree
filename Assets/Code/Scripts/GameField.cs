cfe8a434c9ee292b6bbd43c07957ad81a81bbe61
Yury Kryvoruchko <django.python4132@gmail.com>
2024-07-08 18:50:47 +0300

    Added the InputService.cs.

--- Assets/Code/Scripts/GameField.cs
+++ Assets/Code/Scripts/GameField.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Cysharp.Threading.Tasks;
-
 public class GameField : MonoBehaviour
 {
     [Header("Map Settings")]
---
+++ Assets/Code/Scripts/InputService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputService : MonoBehaviour
+{
+    [SerializeField] private float _swipeResistance;
+    [SerializeField] private InputAction _positionAction;
+    [SerializeField] private InputAction _pressAction;
+
+    private Vector2 _initialPosition;
+
+    private Vector2 CurrentPosition { get => _positionAction.ReadValue<Vector2>(); }
+
+    public event Action<Vector2> OnSwipe;
+
+    private void OnEnable()
+    {
+        _positionAction.Enable();
+        _pressAction.Enable();
+        _pressAction.performed += _ => { _initialPosition = CurrentPosition; };
+        _pressAction.canceled += _ => DetectSwipe();
+    }
+    private void OnDisable()
+    {
+        _positionAction.Disable();
+        _pressAction.Disable();
+    }
+
+    private void DetectSwipe()
+    {
+        Vector2 delta = CurrentPosition - _initialPosition;
+        Vector2 direction = Vector2.zero;
+
+        if (Mathf.Abs(delta.x) > _swipeResistance)
+            direction.x = delta.x;
+        if (Mathf.Abs(delta.y) > _swipeResistance)
+            direction.y = delta.y;
+        if(direction != Vector2.zero)
+            OnSwipe?.Invoke(direction);
+    }
+}
