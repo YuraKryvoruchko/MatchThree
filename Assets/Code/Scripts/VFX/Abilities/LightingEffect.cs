e814fec8a4c2b7bb581d7fb89c046453915fa0fa
Yurii Kryvoruchko <django.python4132@gmail.com>
2024-07-17 15:03:40 +0300

    Added the LightningBolt asset; Changed the code of LightningBoltScript.cs; Added the TestScene; Added the LightingEffect.cs.

---
+++ Assets/Code/Scripts/VFX/Abilities/LightingEffect.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using DigitalRuby.LightningBolt;
+
+namespace Core.VFX.Abilities
+{
+    public class LightingEffect : MonoBehaviour
+    {
+        [SerializeField] private LightningBoltScript _lightningBoltPrefab;
+        [SerializeField] private Sprite _asda;
+
+        private bool _isPaused;
+
+        private void Start()
+        {
+            Play(Vector3.zero, Vector3.down * 10);
+        }
+
+        public void Play(Vector3 startPosition, Vector3 endPosition)
+        {
+            LightningBoltScript lightningBolt = _lightningBoltPrefab;//Instantiate(_lightningBoltPrefab, this.transform);
+            lightningBolt.StartPosition = startPosition;
+            lightningBolt.EndPosition = endPosition;
+            lightningBolt.Trigger();
+        }
+        public void Pause(bool pause)
+        {
+            
+        }
+    }
+}
