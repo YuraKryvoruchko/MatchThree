9a7e73945fdf35777d2d955f097d48edc1191d54
Yury Kryvoruchko <django.python4132@gmail.com>
2024-07-19 16:52:52 +0300

    Fixed SupperAbilityEffect.cs.

--- Assets/Code/Scripts/VFX/Abilities/SupperAbilityEffect.cs
+++ Assets/Code/Scripts/VFX/Abilities/SupperAbilityEffect.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 using Cysharp.Threading.Tasks;
+#if UNITY_EDITOR
 using com.cyborgAssets.inspectorButtonPro;
+#endif
 using Core.Extensions;
 
 namespace Core.VFX.Abilities
@@ -12,21 +13,21 @@ namespace Core.VFX.Abilities
     {
         [Header("Settings")]
         [SerializeField] private float _lineSpeed;
-
-        [SerializeField] private AssetReference _explosionPrefabReference;
+        [SerializeField] private ParticleSystem _explosionPrefab;
         [SerializeField] private LineRenderer _linePrefab;
         [Header("Components")]
         [SerializeField] private AudioSource _audioSource;
 
         private List<ParticleSystem> _particles;
 
+#if UNITY_EDITOR
         [ProPlayButton]
         private void Test(Vector3 point1, Vector3 point2)
         {
             Play(new Vector3[] { point1, point2 }, () => Debug.Log("end"));
         }
-
-        public async void Play(Vector3[] endPositions, Action OnReadyLine = null)
+#endif
+        public async void Play(Vector3[] endPositions, Action OnReady = null)
         {
             _audioSource.Play();
             UniTask[] moveTasks = new UniTask[endPositions.Length];
@@ -38,13 +39,13 @@ namespace Core.VFX.Abilities
             }
 
             await moveTasks;
-            OnReadyLine?.Invoke();
+            OnReady?.Invoke();
 
             _particles = new List<ParticleSystem>(endPositions.Length);
             for (int i = 0; i < endPositions.Length; i++)
             {
-                ParticleSystem particle = (await Addressables.InstantiateAsync(_explosionPrefabReference, this.transform))
-                    .GetComponent<ParticleSystem>();
+                ParticleSystem particle = Instantiate(_explosionPrefab, this.transform);
+                particle.transform.position = endPositions[i];
                 particle.Play();
                 _particles.Add(particle);
             }
@@ -54,6 +55,13 @@ namespace Core.VFX.Abilities
         public void Pause(bool pause)
         {
 
+            for(int i = 0; i < _particles.Count; i++)
+            {
+                if (pause)
+                    _particles[i].Pause();
+                else
+                    _particles[i].Play();
+            }
         }
     }
 }
