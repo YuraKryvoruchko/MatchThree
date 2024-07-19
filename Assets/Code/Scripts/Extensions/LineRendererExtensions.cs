2b5746fbdfc46154bad5ec27f3535d8c35cf7d23
Yury Kryvoruchko <django.python4132@gmail.com>
2024-07-19 16:13:32 +0300

    Added LineRendererExtensions.cs to the Assets/Code/Scripts/Extensions directory; Added the CFXR3 Hit Light B (Air) Variant.prefab as variant of CFXR3 Hit Light B (Air).prefab.

---
+++ Assets/Code/Scripts/Extensions/LineRendererExtensions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+namespace Core.Extensions
+{
+    public static class LineRendererExtensions
+    {
+        public static void MoveTo(this LineRenderer line, int index, Vector3 to, float speed)
+        {
+            MoveToAsync(line, index, to, speed).Forget();
+        }
+        public static async UniTask MoveToAsync(this LineRenderer line, int index, Vector3 to, float speed)
+        {
+            Vector3 startPoint = line.GetPosition(0);
+            float progress = 0f;
+            while (progress < 1f)
+            {
+                progress += Time.deltaTime * speed;
+                line.SetPosition(index, Vector3.Lerp(startPoint, to, progress));
+                await UniTask.Yield();
+            }
+            line.SetPosition(index, to);
+        }
+    }
+}
--- Assets/Code/Scripts/VFX/Abilities/SupperAbilityEffect.cs
+++ Assets/Code/Scripts/VFX/Abilities/SupperAbilityEffect.cs
@@ -1,49 +1,59 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
 using Cysharp.Threading.Tasks;
 using com.cyborgAssets.inspectorButtonPro;
+using Core.Extensions;
 
 namespace Core.VFX.Abilities
 {
     public class SupperAbilityEffect : MonoBehaviour
     {
+        [Header("Settings")]
+        [SerializeField] private float _lineSpeed;
+
+        [SerializeField] private AssetReference _explosionPrefabReference;
         [SerializeField] private LineRenderer _linePrefab;
+        [Header("Components")]
         [SerializeField] private AudioSource _audioSource;
+
+        private List<ParticleSystem> _particles;
 
         [ProPlayButton]
         private void Test(Vector3 point1, Vector3 point2)
         {
-            Play(new Vector3[] { point1, point2 });
+            Play(new Vector3[] { point1, point2 }, () => Debug.Log("end"));
         }
 
-        public void Play(Vector3[] endPositions)
+        public async void Play(Vector3[] endPositions, Action OnReadyLine = null)
         {
             _audioSource.Play();
+            UniTask[] moveTasks = new UniTask[endPositions.Length];
             for(int i = 0; i < endPositions.Length; i++)
             {
                 LineRenderer line = Instantiate(_linePrefab, this.transform);
                 line.SetPosition(0, transform.position);
-                line.MoveTo(1, endPositions[i], 0.25f);
+                moveTasks[i] = line.MoveToAsync(1, endPositions[i], _lineSpeed);
             }
+
+            await moveTasks;
+            OnReadyLine?.Invoke();
+
+            _particles = new List<ParticleSystem>(endPositions.Length);
+            for (int i = 0; i < endPositions.Length; i++)
+            {
+                ParticleSystem particle = (await Addressables.InstantiateAsync(_explosionPrefabReference, this.transform))
+                    .GetComponent<ParticleSystem>();
+                particle.Play();
+                _particles.Add(particle);
+            }
+            _particles.Clear();
         }
 
         public void Pause(bool pause)
         {
-            
-        }
-    }
-    public static class LineRendererExtensions
-    {
-        public static async void MoveTo(this LineRenderer line, int index, Vector3 to, float speed)
-        {
-            Vector3 startPoint = line.GetPosition(0);
-            float progress = 0f;
-            while (progress < 1f)
-            {
-                progress += Time.deltaTime * speed;
-                line.SetPosition(index, Vector3.Lerp(startPoint, to, progress));
-                await UniTask.Yield();
-            }
-            line.SetPosition(index, to);
+
         }
     }
 }
