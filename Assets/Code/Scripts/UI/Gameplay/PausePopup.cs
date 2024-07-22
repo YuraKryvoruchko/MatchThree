aab57f0ba4e4e3102be6c3413a137f2cbdd984e1
Yury Kryvoruchko <django.python4132@gmail.com>
2024-07-22 16:51:25 +0300

    Added the PausePopup.cs; Moved and updated the PausePopup.prefab; Moved the CompletePopup.prefab.

---
+++ Assets/Code/Scripts/UI/Gameplay/PausePopup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Core.Infrastructure.UI;
+
+namespace Core.UI.Gameplay
+{
+    public class PausePopup : WindowBase
+    {
+        [Header("Buttons")]
+        [SerializeField] private Button _closeButton;
+
+        public override event Action OnMenuBack;
+
+        protected override void OnShow()
+        {
+            _closeButton.onClick.AddListener(BackMenu);
+        }
+        protected override void OnHide()
+        {
+            _closeButton.onClick.RemoveListener(BackMenu);
+        }
+        protected override void OnFocus()
+        {
+            _closeButton.interactable = true;
+        }
+        protected override void OnUnfocus()
+        {
+            _closeButton.interactable = false;
+        }
+        protected override void OnClose()
+        {
+            OnHide();
+            OnUnfocus();
+        }
+
+        private void BackMenu()
+        {
+            OnMenuBack?.Invoke();
+        }
+    }
+}
