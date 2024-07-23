15c1a4e3fb168b3d1778124c2d5043a8a2e96d5c
Yury Kryvoruchko <django.python4132@gmail.com>
2024-07-23 18:48:02 +0300

    Tested the AudioService on the TestScene.unity; Added the AssetReferenceAudioClip.cs; Added PlayBackgroundMusicByType, PauseBackgroundMusic and UnPauseBackgroundMusic methods to the AudioService.cs.

---
+++ Assets/Code/Scripts/Infrastructure/Services/AudioService/AssetReferenceAudioClip.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Core.Infrastructure.Service
+{
+    [System.Serializable]
+    public class AssetReferenceAudioClip : AssetReferenceT<AudioClip>
+    {
+        public AssetReferenceAudioClip(string guid) : base(guid)
+        {
+        }
+    }
+}
--- Assets/Code/Scripts/Infrastructure/Services/AudioService/AudioService.cs
+++ Assets/Code/Scripts/Infrastructure/Services/AudioService/AudioService.cs
@@ -1,12 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using com.cyborgAssets.inspectorButtonPro;
 
 namespace Core.Infrastructure.Service
 {
-    public class AudioService
+    public enum BackgroundMusicType
     {
-        private AudioSource _backgroundSource;
-       
+        MainMenu,
+        LoadingScreen,
+        Gameplay
+    }
+    public class AudioService : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _backgroundSoundSource;
+        [SerializeField] private AudioListByType[] _audioClips;
+
+        private BackgroundMusicType _currentMusicType;
+        private int _currentClipIndex;
+
+        [System.Serializable]
+        private struct AudioListByType
+        {
+#if UNITY_EDITOR
+            public string InspectorName;
+#endif
+            public BackgroundMusicType Type;
+            public AssetReferenceAudioClip[] Clips;
+        }
+
+        [ProPlayButton]
+        public async void PlayBackgroundMusicByType(BackgroundMusicType backgroundMusicType)
+        {
+            if(_currentMusicType != backgroundMusicType)
+            {
+                UnloadMusicByType(_currentMusicType);
+                _currentMusicType = backgroundMusicType;
+                _currentClipIndex = 0;
+            }
+            
+            foreach (AudioListByType list in _audioClips)
+            {
+                if (list.Type != _currentMusicType)
+                    continue;
+                if (_currentClipIndex >= list.Clips.Length)
+                    _currentClipIndex = 0;
+
+                if(list.Clips[_currentClipIndex].Asset == null)
+                {
+                    if (!list.Clips[_currentClipIndex].IsDone && list.Clips[_currentClipIndex].OperationHandle.PercentComplete < 1f)
+                        await list.Clips[_currentClipIndex].OperationHandle.Task;
+                    else
+                        await list.Clips[_currentClipIndex].LoadAssetAsync().Task;
+                }
+                Debug.Log(list.Type);
+                _backgroundSoundSource.clip = list.Clips[_currentClipIndex].Asset as AudioClip;
+                _backgroundSoundSource.Play();
+                _currentMusicType++;
+                break;
+            }
+        }
+        [ProPlayButton]
+        public void PauseBackgroundMusic()
+        {
+            _backgroundSoundSource.Pause();
+        }
+        [ProPlayButton]
+        public void UnPauseBackgroundMusic()
+        {
+            _backgroundSoundSource.UnPause();
+        }
+
+        private void UnloadMusicByType(BackgroundMusicType backgroundMusicType)
+        {
+            foreach (AudioListByType list in _audioClips)
+            {
+                if (list.Type != _currentMusicType)
+                    continue;
+
+                for(int i = 0; i < list.Clips.Length; i++)
+                    list.Clips[i].ReleaseAsset();
+                break;
+            }
+        }
     }
 }
