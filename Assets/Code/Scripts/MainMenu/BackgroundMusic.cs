3ae3fe01da2bae8fb36a28e8bffe302956c59e02
Yury Kryvoruchko <django.python4132@gmail.com>
2024-08-06 17:29:07 +0300

    Renamed the AudioSceneSetup.cs to BackgroundMusic.cs. Added ReleaseSource call to BackgroundMusic.cs on destroy object.

---
+++ Assets/Code/Scripts/MainMenu/BackgroundMusic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Zenject;
+using Core.Infrastructure.Service.Audio;
+
+namespace Core.MainMenu
+{
+    public class BackgroundMusic : MonoBehaviour
+    {
+        [SerializeField] private ClipEvent _event;
+
+        private IAudioService _audioService;
+        private SourceInstance _sourceInstance;
+
+        [Inject]
+        private void Construct(IAudioService audioService)
+        {
+            _audioService = audioService;
+            _sourceInstance = _audioService.PlayWithSource(_event);
+        }
+
+        private void OnDestroy()
+        {
+            _audioService.ReleaseSource(_sourceInstance);
+        }
+    }
+}
