ce47ecd1a9478c965aeaf10eb40579ed759fbcc1
Yury Kryvoruchko <django.python4132@gmail.com>
2024-08-09 16:32:56 +0300

    Fixed the SupperAbilityEffect.cs; Fixed the SuperAbilityEffect.prefab.

--- Assets/Code/Scripts/VFX/Abilities/SupperAbilityEffect.cs
+++ Assets/Code/Scripts/VFX/Abilities/SupperAbilityEffect.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
-using DG.Tweening;
-using Unity.VisualScripting;
 
 #if UNITY_EDITOR
 using com.cyborgAssets.inspectorButtonPro;
