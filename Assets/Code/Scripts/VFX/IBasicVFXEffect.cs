6af9ffa50d5435f781ea332585131ff1c6f575d1
Yury Kryvoruchko <django.python4132@gmail.com>
2024-08-19 13:53:07 +0300

    Added IBasicVFXEffect and IVFXEffect intefaces for VFX's scripts; Added TestVFXEffect.cs for testing.

---
+++ Assets/Code/Scripts/VFX/IBasicVFXEffect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+#if UNITY_EDITOR
+#endif
+
+namespace Core.VFX.Abilities
+{
+    public interface IBasicVFXEffect
+    {
+        UniTask Play();
+        void Pause(bool isPause);
+        void Stop();
+
+        event Action OnEnd;
+    }
+}
---
+++ Assets/Code/Scripts/VFX/IVFXEffect.cs
@@ -0,0 +1,10 @@
+﻿#if UNITY_EDITOR
+#endif
+
+namespace Core.VFX.Abilities
+{
+    public interface IVFXEffect<T> : IBasicVFXEffect
+    {
+        void SetParameters(T parameters);
+    }
+}
