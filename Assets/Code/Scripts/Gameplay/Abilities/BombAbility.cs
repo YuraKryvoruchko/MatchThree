46c7c942c55ca04a1dd8e45f3bec0e4287bb686a
Yury Kryvoruchko <django.python4132@gmail.com>
2024-08-20 19:12:30 +0300

    Added ExplosiveVFXEffect.cs and added this component to SmallBombEffect.prefab; Modified BombAbility.cs for ExplosiveVFXEffect.

--- Assets/Code/Scripts/Gameplay/Abilities/BombAbility.cs
+++ Assets/Code/Scripts/Gameplay/Abilities/BombAbility.cs
@@ -1,14 +1,15 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using Core.Infrastructure.Service.Audio;
+using Core.VFX;
 
 namespace Core.Gameplay
 {
     public class BombAbility : IAbility
     {
         private AssetReference _bombEffectReference;
-        private ParticleSystem _bombEffectInstance;
 
         private int _lineLenght = 5;
 
@@ -17,6 +18,8 @@ namespace Core.Gameplay
         private IAudioService _audioService;
         private ClipEvent _explosiveEvent;
         private SourceInstance _audioSourceInstance;
+
+        private event Action<bool> OnPause;
 
         public BombAbility(int lineLenght, IAudioService audioService, ClipEvent explosiveEvent, AssetReference bombEffectReference)
         {
@@ -32,19 +35,20 @@ namespace Core.Gameplay
         }
         void IAbility.SetPause(bool isPause)
         {
-            if (isPause)
-                _bombEffectInstance.Pause();
-            else
-                _bombEffectInstance.Play();
-
             _audioSourceInstance.Pause(isPause);
+            OnPause?.Invoke(isPause);
         }
         async UniTask IAbility.Execute(Vector2Int swipedCellPosition, Vector2Int abilityPosition)
         {
             Vector3 cellPosition = _gameField.CellPositionToWorld(abilityPosition);
-            _bombEffectInstance = (await Addressables.InstantiateAsync(_bombEffectReference,
-                cellPosition, Quaternion.identity)).GetComponent<ParticleSystem>();
-            _bombEffectInstance.Play();
+            IBasicVFXEffect bombVFXEffect = (await Addressables.InstantiateAsync(_bombEffectReference,
+                cellPosition, Quaternion.identity)).GetComponent<IBasicVFXEffect>();
+
+            bombVFXEffect.OnComplete += ReleaseEffect;
+            bombVFXEffect.OnStoped += ReleaseEffect;
+            OnPause += bombVFXEffect.Pause;
+            bombVFXEffect.Play().Forget();
+
             _audioSourceInstance = _audioService.PlayWithSource(_explosiveEvent);
 
             int lenghtFromBombCell = (_lineLenght - 1) / 2, taskArrayIndex = 0;
@@ -59,7 +63,14 @@ namespace Core.Gameplay
 
             await UniTask.WhenAll(explodeTasks);
             _audioService.ReleaseSource(_audioSourceInstance);
-            Addressables.ReleaseInstance(_bombEffectInstance.gameObject);
+        }
+
+        private void ReleaseEffect(IBasicVFXEffect effect)
+        {
+            effect.OnComplete -= ReleaseEffect;
+            effect.OnStoped -= ReleaseEffect;
+            OnPause -= effect.Pause;
+            Addressables.ReleaseInstance((effect as MonoBehaviour).gameObject);
         }
     }
 }
---
+++ Assets/Code/Scripts/VFX/Abilities/ExplosiveVFXEffect.cs
@@ -0,0 +1,52 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using System;
+
+namespace Core.VFX
+{
+    public class ExplosiveVFXEffect : MonoBehaviour, IVFXEffect<ExplosiveVFXEffect.ExplosiveVFXEffectParameters>
+    {
+        [SerializeField] private ParticleSystem _explosiveEffectInstance;
+
+        public event Action<IBasicVFXEffect> OnStart;
+        public event Action<IBasicVFXEffect, bool> OnPause;
+        public event Action<IBasicVFXEffect> OnComplete;
+        public event Action<IBasicVFXEffect> OnStoped;
+
+        public class ExplosiveVFXEffectParameters
+        {
+        }
+
+        public void Pause(bool isPause)
+        {
+            if (isPause)
+                _explosiveEffectInstance.Pause();
+            else
+                _explosiveEffectInstance.Play();
+
+            OnPause?.Invoke(this, isPause);
+        }
+
+        public async UniTask Play()
+        {
+            OnStart?.Invoke(this);
+
+            _explosiveEffectInstance.Play();
+            await UniTask.WaitWhile(() => _explosiveEffectInstance.time < _explosiveEffectInstance.main.duration || !_explosiveEffectInstance.isStopped);
+
+            if(_explosiveEffectInstance.isStopped)
+                OnStoped?.Invoke(this);
+            else
+                OnComplete?.Invoke(this);
+        }
+
+        public void SetParameters(ExplosiveVFXEffectParameters parameters = null)
+        {
+        }
+
+        public void Stop()
+        {
+            _explosiveEffectInstance.Stop();
+        }
+    }
+}
