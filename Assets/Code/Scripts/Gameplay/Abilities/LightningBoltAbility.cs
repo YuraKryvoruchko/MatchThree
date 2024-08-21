eaff98fc73efaadec6f167d345640f59330d5d9d
Yury Kryvoruchko <django.python4132@gmail.com>
2024-08-21 13:28:56 +0300

    Modified LightningBoltAbility for new pausing mechanism.

--- Assets/Code/Scripts/Gameplay/Abilities/LightningBoltAbility.cs
+++ Assets/Code/Scripts/Gameplay/Abilities/LightningBoltAbility.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.AddressableAssets;
 using Cysharp.Threading.Tasks;
 using Core.VFX.Abilities;
 using Core.Infrastructure.Service.Audio;
 using System.Collections.Generic;
+
+using Random = UnityEngine.Random;
 
 namespace Core.Gameplay
 {
@@ -11,14 +14,18 @@ namespace Core.Gameplay
     {
         private GameField _gameField;
         private AssetReference _lightingBoltEffectPrefab;
-        private List<LightingBoltEffect> _lightingBoltEffects;
 
         private int _lightningBoltCount;
         private IAbility _severalAbility;
 
         private IAudioService _audioService;
         private ClipEvent _clipEvent;
-        private List<SourceInstance> _audioSourceInstancies;
+
+        private bool _isPaused;
+
+        private event Action<bool> OnPause;
+
+        private const float LIGHTNING_DELAY = 0.4F;
 
         public LightningBoltAbility(IAudioService audioService, ClipEvent hitClipEvent, AssetReference lightingBoltEffectPrefab,
             int lightningBoltCount, IAbility severalAbility = null)
@@ -28,29 +35,27 @@ namespace Core.Gameplay
             _lightingBoltEffectPrefab = lightingBoltEffectPrefab;
             _lightningBoltCount = lightningBoltCount;
             _severalAbility = severalAbility;
-
-            _audioSourceInstancies = new List<SourceInstance>();
-            _lightingBoltEffects = new List<LightingBoltEffect>();
         }
 
-        void IAbility.Init(GameField gameField)
+        public void Init(GameField gameField)
         {
+            if (_gameField != null)
+                _gameField.OnPause -= SetPause;
+
             _gameField = gameField;
+            _gameField.OnPause += SetPause;
         }
-        void IAbility.SetPause(bool isPause)
+        public void SetPause(bool isPause)
         {
-            foreach(LightingBoltEffect effect in _lightingBoltEffects)
-                effect.Pause(isPause);
-            foreach(SourceInstance source in _audioSourceInstancies)
-                source.Pause(isPause);
+            _isPaused = isPause;
+            OnPause?.Invoke(isPause);
         }
-        async UniTask IAbility.Execute(Vector2Int swipedCellPosition, Vector2Int abilityPosition)
+        public async UniTask Execute(Vector2Int swipedCellPosition, Vector2Int abilityPosition)
         {
             if (_severalAbility != null)
                 _severalAbility.Init(_gameField);
 
             SourceInstance audioInstance = _audioService.PlayWithSource(_clipEvent, false);
-            _audioSourceInstancies.Add(audioInstance);
 
             Cell swipedCell = _gameField.GetCell(swipedCellPosition);
             Cell abilityCell = _gameField.GetCell(abilityPosition);
@@ -63,7 +68,9 @@ namespace Core.Gameplay
 
             LightingBoltEffect lightingBoltEffect = (await Addressables.InstantiateAsync(_lightingBoltEffectPrefab))
                 .GetComponent<LightingBoltEffect>();
-            _lightingBoltEffects.Add(lightingBoltEffect);
+
+            OnPause += lightingBoltEffect.Pause;
+            OnPause += audioInstance.Pause;
 
             Cell randomCell = swipedCell;
             for (int i = 0; i < _lightningBoltCount && cells.Count > 0; i++)
@@ -76,17 +83,21 @@ namespace Core.Gameplay
                 lightingBoltEffect.Play(startPosition, randomCell.transform.position);
                 audioInstance.Play();
                 if (_severalAbility == null)
-                    await _gameField.ExplodeCellAsync(_gameField.WorldPositionToCell(randomCell.transform.position));
+                    _gameField.ExplodeCellAsync(_gameField.WorldPositionToCell(randomCell.transform.position)).Forget();
                 else
-                    await _severalAbility.Execute(swipedCellPosition, _gameField.WorldPositionToCell(randomCell.transform.position));
+                    _severalAbility.Execute(swipedCellPosition, _gameField.WorldPositionToCell(randomCell.transform.position)).Forget();
 
                 if(cells.Count > 0)
                     randomCell = cells[Random.Range(0, cells.Count - 1)];
+
+                await UniTask.WaitForSeconds(LIGHTNING_DELAY);
+                if (_isPaused)
+                    await UniTask.WaitWhile(() => _isPaused);
             }
 
-            _lightingBoltEffects.Remove(lightingBoltEffect);
+            OnPause -= lightingBoltEffect.Pause;
+            OnPause -= audioInstance.Pause;
             Addressables.ReleaseInstance(lightingBoltEffect.gameObject);
-            _audioSourceInstancies.Remove(audioInstance);
             _audioService.ReleaseSource(audioInstance);
         }
     }
