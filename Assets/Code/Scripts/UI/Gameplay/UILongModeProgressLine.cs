e279399a56ca7006cde5fc4829b8accf8a36df9b
Yury Kryvoruchko <django.python4132@gmail.com>
2024-08-29 11:55:53 +0300

    Added star showing when the progress reaches a certain value.

--- Assets/Code/Scripts/UI/Gameplay/UILongModeProgressLine.cs
+++ Assets/Code/Scripts/UI/Gameplay/UILongModeProgressLine.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
 using DG.Tweening;
@@ -12,8 +13,12 @@ namespace Core.UI.Gameplay
     {
         [Header("Settings")]
         [SerializeField] private float _updateScoreDelay = 0.25f;
+        [Space]
+        [SerializeField] private bool _showStars;
+        [SerializeField] private float _starShowingDelay = 0.25f;
         [Header("Components")]
         [SerializeField] private Slider _slider;
+        [SerializeField] private StarSettings[] _stars;
 
         private int _recordValue;
 
@@ -21,6 +26,16 @@ namespace Core.UI.Gameplay
 
         private GameScoreTracking _gameScoreTracking;
         private IPauseProvider _pauseProvider;
+
+        [Serializable]
+        private class StarSettings
+        {
+            public GameObject Star;
+            public float MinProgress;
+
+            [NonSerialized]
+            public Tweener Tweener;
+        }
 
         [Inject]
         private void Construct(GameScoreTracking gameScoreTracking, ISavingService savingService, IPauseProvider pauseProvider)
@@ -44,17 +59,39 @@ namespace Core.UI.Gameplay
         
         private void HandleUpdateScoreCount(int scoreCount)
         {
+            float progress = (float)scoreCount / (float)_recordValue;
             if (_tweener.IsActive())
-                _tweener.ChangeEndValue((float)scoreCount / (float)_recordValue, _updateScoreDelay, true);
+                _tweener.ChangeEndValue(progress, _updateScoreDelay, true);
             else
-                _tweener = _slider.DOValue((float)scoreCount / (float)_recordValue, _updateScoreDelay);
+                _tweener = _slider.DOValue(progress, _updateScoreDelay);
+
+            if (_showStars)
+            {
+                for(int i = 0; i < _stars.Length; i++)
+                {
+                    if (_stars[i].MinProgress <= progress && !_stars[i].Star.activeSelf)
+                    {
+                        _stars[i].Star.SetActive(true);
+                        _stars[i].Star.transform.localScale = Vector3.zero;
+                        _stars[i].Tweener = _stars[i].Star.transform.DOScale(Vector3.one, _starShowingDelay).SetEase(Ease.OutBack);
+                    }
+                }
+            }
         }
         private void HandlePause(bool isPause)
         {
             if (isPause)
-                _slider.DOPause();
+            {
+                _tweener.Pause();
+                for (int i = 0; i < _stars.Length; i++)
+                    _stars[i].Tweener.Pause();
+            }
             else
-                _slider.DOPlay();
+            {
+                _tweener.Play();
+                for (int i = 0; i < _stars.Length; i++)
+                    _stars[i].Tweener.Play();
+            }
         }
     }
 }
