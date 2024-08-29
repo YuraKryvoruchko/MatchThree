cb4d93ff497eccb951ad8aa98b82d6d416128c4f
Yury Kryvoruchko <django.python4132@gmail.com>
2024-08-29 18:19:54 +0300

    Added MoveCount field to LevelConfig.cs; Added GameProgressObserver.cs.

---
+++ Assets/Code/Scripts/Infrastructure/Gameplay/GameProgressObserver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using Core.Gameplay;
+
+namespace Core.Infrastructure.Gameplay
+{
+    public class GameProgressObserver : IDisposable
+    {
+        private GameField _gameField;
+
+        private LevelConfig _levelConfig;
+        private LevelTaskCompletionChecker _taskCompletionChecker;
+
+        private int _moveCount;
+
+        public GameProgressObserver(LevelConfig levelConfig, GameField gameField, LevelTaskCompletionChecker levelTaskCompletionChecker)
+        {
+            _levelConfig = levelConfig;
+
+            _gameField = gameField;
+            _gameField.OnMove += HandleMoveOnField;
+
+            _taskCompletionChecker = levelTaskCompletionChecker;
+            _taskCompletionChecker.OnAllTaskCompleted += HandleTaskCompleting;
+        }
+        public void Dispose()
+        {
+            _gameField.OnMove -= HandleMoveOnField;
+            _taskCompletionChecker.OnAllTaskCompleted -= HandleTaskCompleting;
+        }
+
+        private void HandleMoveOnField()
+        {
+            _moveCount++;
+            if (_moveCount != _levelConfig.MoveCount)
+                return;
+                
+            Debug.Log("Game Over!");
+        }
+        private void HandleTaskCompleting()
+        {
+            Debug.Log("Complete!");
+        }
+    }
+}
--- Assets/Code/Scripts/Infrastructure/Gameplay/LevelConfig.cs
+++ Assets/Code/Scripts/Infrastructure/Gameplay/LevelConfig.cs
@@ -6,6 +6,9 @@ namespace Core.Infrastructure.Gameplay
     [CreateAssetMenu(fileName = "LevelConfig", menuName = "SO/Gameplay/LevelConfig")]
     public class LevelConfig : ScriptableObject
     {
+        [Header("Config")]
+        public int MoveCount;
+        [Header("Tasks")]
         public LevelTask[] Tasks;
     }
 }
