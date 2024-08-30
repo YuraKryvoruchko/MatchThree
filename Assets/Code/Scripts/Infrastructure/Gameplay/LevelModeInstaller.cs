9215e1703e495842a7bdd4d14f1bf9a86d5f41e9
Yury Kryvoruchko <django.python4132@gmail.com>
2024-08-30 14:41:47 +0300

    Added LevelModeInstaller.cs; Added LevelModeGameplayUI.prefab and LevelModeGameplay scene; Tested LevelModeGameplay.

---
+++ Assets/Code/Scripts/Infrastructure/Gameplay/LevelModeInstaller.cs
@@ -0,0 +1,39 @@
+﻿using Core.Gameplay;
+using UnityEngine;
+using Zenject;
+
+namespace Core.Infrastructure.Gameplay
+{
+    public class LevelModeInstaller : MonoInstaller
+    {
+        [SerializeField] private LevelConfig _levelConfig;
+
+        public override void InstallBindings()
+        {
+            BindLevelTaskCompletionChecker();
+            BindPlayerMoveObserver();
+            BindGameProgressObserver();
+        }
+        
+        private void BindLevelTaskCompletionChecker()
+        {
+            Container
+                .BindInterfacesAndSelfTo<LevelTaskCompletionChecker>()
+                .AsSingle()
+                .WithArguments(_levelConfig.Tasks);
+        }
+        private void BindPlayerMoveObserver()
+        {
+            Container
+                .BindInterfacesAndSelfTo<PlayerMoveObserver>()
+                .AsSingle();
+        }
+        private void BindGameProgressObserver()
+        {
+            Container
+                .BindInterfacesAndSelfTo<GameProgressObserver>()
+                .AsSingle()
+                .WithArguments(_levelConfig);
+        }
+    }
+}
