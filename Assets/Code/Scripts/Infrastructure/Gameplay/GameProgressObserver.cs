92bd3624ba1cd3531888622d54b8b86b788c7dd0
Yury Kryvoruchko <django.python4132@gmail.com>
2024-08-31 16:20:38 +0300

    Tested CompletePopup in GameProgressObserver.cs.

--- Assets/Code/Scripts/Infrastructure/Gameplay/GameProgressObserver.cs
+++ Assets/Code/Scripts/Infrastructure/Gameplay/GameProgressObserver.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using UnityEngine;
 using Core.Gameplay;
+using Core.Infrastructure.Service;
+using Cysharp.Threading.Tasks;
+using Core.UI.Gameplay;
 
 namespace Core.Infrastructure.Gameplay
 {
@@ -10,10 +13,13 @@ namespace Core.Infrastructure.Gameplay
         private LevelTaskCompletionChecker _taskCompletionChecker;
         private PlayerMoveObserver _playerMoveObserver;
 
+        private IWindowService _windowService;
+
         public event Action OnLose;
         public event Action<int> OnComplete;
 
-        public GameProgressObserver(LevelConfig levelConfig, PlayerMoveObserver playerMoveObserver, LevelTaskCompletionChecker levelTaskCompletionChecker)
+        public GameProgressObserver(LevelConfig levelConfig, PlayerMoveObserver playerMoveObserver, LevelTaskCompletionChecker levelTaskCompletionChecker,
+            IWindowService windowService)
         {
             _levelConfig = levelConfig;
 
@@ -22,6 +28,8 @@ namespace Core.Infrastructure.Gameplay
 
             _taskCompletionChecker = levelTaskCompletionChecker;
             _taskCompletionChecker.OnAllTaskCompleted += HandleTaskCompleting;
+
+            _windowService = windowService;
         }
         public void Dispose()
         {
@@ -33,11 +41,23 @@ namespace Core.Infrastructure.Gameplay
         {
             if (_playerMoveObserver.Count != _levelConfig.MoveCount)
                 return;
-            
+
+            UniTask.Void(async () =>
+            {
+                CompletePopup completePopup = await _windowService.OpenPopup<CompletePopup>("CompletePopup");
+                completePopup.Activate(_taskCompletionChecker.GetProgress(), 3839).Forget();
+            });
+
             Debug.Log("Game Over!");
         }
         private void HandleTaskCompleting()
         {
+            UniTask.Void(async () =>
+            {
+                CompletePopup completePopup = await _windowService.OpenPopup<CompletePopup>("CompletePopup");
+                completePopup.Activate(1f, 3839).Forget();
+            });
+
             Debug.Log("Complete!");
         }
     }
