5620754cc994d28262b8775b3020e745028b2a36
Yury Kryvoruchko <django.python4132@gmail.com>
2024-09-01 12:41:22 +0300

    Added SelectLevelButton.cs; Added SelectLevelMenu.cs; Updated LevelSelectionButton.prefab.

---
+++ Assets/Code/Scripts/UI/MainMenu/SelectLevelButton.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+namespace Core.UI
+{
+    public class SelectLevelButton : MonoBehaviour
+    {
+        [SerializeField] private Button _baseButton;
+        [SerializeField] private TMP_Text _levelNumberText;
+        [SerializeField] private StarSettings[] _starSettings;
+
+        public int LevelIndex { get; private set; }
+
+        public event Action<SelectLevelButton> OnClick;
+
+        [Serializable]
+        private class StarSettings
+        {
+            public Image Image;
+            public float MinProgress;
+        }
+
+        private void Awake()
+        {
+            _baseButton.onClick.AddListener(() => OnClick?.Invoke(this));
+        }
+        private void OnDestroy()
+        {
+            _baseButton.onClick.RemoveAllListeners();
+        }
+
+        public void SetLevelIndex(int index)
+        {
+            LevelIndex = index;
+            _levelNumberText.text = (LevelIndex + 1).ToString();
+        }
+        public void SetLevelProgress(float progress)
+        {
+            for(int i = 0; i < _starSettings.Length; i++)
+            {
+                if (_starSettings[i].MinProgress > progress)
+                    break;
+
+                Color color = _starSettings[i].Image.color;
+                color.a = 1f;
+                _starSettings[i].Image.color = color;
+            }
+        }
+    }
+}
---
+++ Assets/Code/Scripts/UI/MainMenu/SelectLevelMenu.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+using Core.Infrastructure.Service;
+using Core.Infrastructure.UI;
+using Core.Infrastructure.Service.Audio;
+using Core.Infrastructure.Loading;
+using Core.Infrastructure.Service.Saving;
+
+namespace Core.UI
+{
+    public class SelectLevelMenu : WindowBase
+    {
+        [Header("Buttons")]
+        [SerializeField] private Button _backButton;
+        [Header("Select Button Settings")]
+        [SerializeField] private SelectLevelButton _selectLevelButtonPrefab;
+        [SerializeField] private Transform _buttonContainer;
+        [Header("Audio Keys")]
+        [SerializeField] private ClipEvent _clickAudioEvent;
+
+        private SelectLevelButton[] _buttons;
+
+        private ILoadingScreenProvider _loadingScreenProvider;
+
+        private SceneService _sceneService;
+        private IAudioService _audioService;
+        private ILevelService _levelService;
+        private ISavingService _savingService;
+
+        public override event Action OnMenuBack;
+
+        [Inject]
+        private void Construct(SceneService sceneService, IAudioService audioService, ILevelService levelService, ISavingService savingService,
+            ILoadingScreenProvider loadingScreenProvider)
+        {
+            _sceneService = sceneService;
+            _audioService = audioService;
+            _levelService = levelService;
+            _savingService = savingService;
+            _loadingScreenProvider = loadingScreenProvider;
+        }
+
+        protected override void OnShow()
+        {
+            _backButton.onClick.AddListener(() => OnMenuBack?.Invoke());
+        }
+        protected override void OnHide()
+        {
+            _backButton.onClick.RemoveAllListeners();
+        }
+        protected override void OnFocus()
+        {
+            _backButton.interactable = true;
+        }
+        protected override void OnUnfocus()
+        {
+            _backButton.interactable = false;
+        }
+        protected override void OnClose()
+        {
+            UnsubscribeFromButtons();
+        }
+
+        public void ShowButtons()
+        {
+            int levelCount = _levelService.LevelConfigCount;
+            _buttons = new SelectLevelButton[levelCount];
+            for (int i = 0; i < levelCount; i++)
+            {
+                _buttons[i] = Instantiate(_selectLevelButtonPrefab, _buttonContainer);
+                _buttons[i].SetLevelIndex(i);
+                _buttons[i].SetLevelProgress(_savingService.GetLevelProgress(i));
+                _buttons[i].OnClick += HandleClick;
+            }
+        }
+
+        private void HandleClick(SelectLevelButton button)
+        {
+            _audioService.PlayOneShot(_clickAudioEvent);
+
+            UnsubscribeFromButtons();
+            Debug.Log($"Load level with index {button.LevelIndex}");
+        }
+        private void UnsubscribeFromButtons()
+        {
+            if (_buttons == null)
+                return;
+
+            int levelCount = _levelService.LevelConfigCount;
+            for (int i = 0; i < levelCount; i++)
+            {
+                _buttons[i].OnClick -= HandleClick;
+            }
+        }
+    }
+}
