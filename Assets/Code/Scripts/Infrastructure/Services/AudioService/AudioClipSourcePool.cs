86878c83c63a934cc8afebc51d40854a32d0f090
Yurii Kryvoruchko <django.python4132@gmail.com>
2024-09-04 20:27:23 +0300

    Fixed creating new audio source in AudioClipSourcePool.cs.

--- Assets/Code/Scripts/Infrastructure/Services/AudioService/AudioClipSourcePool.cs
+++ Assets/Code/Scripts/Infrastructure/Services/AudioService/AudioClipSourcePool.cs
@@ -12,6 +12,7 @@ namespace Core.Infrastructure.Service.Audio
         private Transform _audioSourceContainer;
 
         private int _startCapacity;
+        private int _numberSource = 0;
 
         public AudioClipSourcePool(Transform audioSourceContainer, int startCapacity = 15)
         {
@@ -35,6 +36,9 @@ namespace Core.Infrastructure.Service.Audio
         public AudioClipSource GetAudioClipSource(ClipEvent clipEvent, AudioMixerGroup mixerGroup = null, 
             float spatialBlend = 0, Vector3 position = default)
         {
+            if (_audioClipSources.Count == 0)
+                CreateNewAudioClipSource();
+
             AudioClipSource source = _audioClipSources.Dequeue();
             source.Init(clipEvent, mixerGroup, spatialBlend, position);
             return source;
@@ -47,9 +51,9 @@ namespace Core.Infrastructure.Service.Audio
 
         private void CreateNewAudioClipSource()
         {
-            for (int i = 0; i < _startCapacity; i++)
+            for (int i = 0; i < _startCapacity; i++, _numberSource++)
             {
-                AudioSource audioSource = new GameObject("AudioSource " + i).AddComponent<AudioSource>();
+                AudioSource audioSource = new GameObject("AudioSource " + _numberSource).AddComponent<AudioSource>();
                 audioSource.transform.parent = _audioSourceContainer;
                 audioSource.playOnAwake = false;
                 audioSource.gameObject.SetActive(false);
