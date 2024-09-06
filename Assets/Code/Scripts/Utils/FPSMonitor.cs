8159b841e6411d7dc8f00ce5aa7d13f5bdc7a587
Yury Kryvoruchko <django.python4132@gmail.com>
2024-09-06 16:38:01 +0300

    Implemented FPSMonitor.cs; Added FpsCanvas in the Boot.unity scene.

--- Assets/Code/Scripts/Utils/FPSMonitor.cs
+++ Assets/Code/Scripts/Utils/FPSMonitor.cs
@@ -1,9 +1,17 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 namespace Core.Utils
 {
     public class FPSMonitor : MonoBehaviour
     {
+        [SerializeField] private TMP_Text _fpsText;
+        [SerializeField] private TMP_Text _trargetFrameRateText;
+
+        private string _fpsTextDescription;
+        private string _targetFrameRateTextDescription;
+
         private int _frameRate;
 
         private float _time;
@@ -11,14 +19,30 @@ namespace Core.Utils
 
         private const float POLLING_TIME = 1F;
 
+        private void Start()
+        {
+            _fpsTextDescription = _fpsText.text;
+            _targetFrameRateTextDescription = _trargetFrameRateText.text;
+        }
         private void Update()
+        {
+            UpdateTargetFrameRate();
+            UpdateCurrentFrameRate();
+        }
+
+        private void UpdateTargetFrameRate()
+        {
+            _trargetFrameRateText.text = _targetFrameRateTextDescription + Math.Round(Screen.currentResolution.refreshRateRatio.value).ToString();
+        }
+        private void UpdateCurrentFrameRate()
         {
             _time += Time.deltaTime;
             _frameCount++;
 
-            if( _time >= POLLING_TIME)
+            if (_time >= POLLING_TIME)
             {
                 _frameRate = Mathf.RoundToInt(_frameCount / _time);
+                _fpsText.text = _fpsTextDescription + _frameRate.ToString();
 
                 _time -= POLLING_TIME;
                 _frameCount = 0;
